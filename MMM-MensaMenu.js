ecbc892910177e76c781f8db2f9c93899038cfbb
kaza1001 <151996682+kaza1001@users.noreply.github.com>
2023-12-07 14:14:34 +0100

    Add files via upload

--- MMM-MensaMenu.js
+++ MMM-MensaMenu.js
@@ -1,13 +1,15 @@
 Module.register("MMM-MensaMenu", {
-  // Default module config.
-  defaults: {
-    text: "Du stinkst!",
-  },
+    // Default module config.
+    defaults: {
+      text: abc,
+    },
+  
+    // Override dom generator.
+    getDom: function () {
+      var wrapper = document.createElement("div");
+      wrapper.innerHTML = this.config.text;
+      return wrapper;
+    },
+  });
 
-  // Override dom generator.
-  getDom: function () {
-    var wrapper = document.createElement("div");
-    wrapper.innerHTML = this.config.text;
-    return wrapper;
-  },
-});
+  const abc = "<p>Hallo</p><p>Ciao</p>"
