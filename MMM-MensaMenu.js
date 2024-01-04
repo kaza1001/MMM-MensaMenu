7fcedd6ae4679d7db42739ac8e41abbe76150690
kaza1001 <151996682+kaza1001@users.noreply.github.com>
2024-01-04 15:45:42 +0100

    Add files via upload

--- MMM-MensaMenu.js
+++ MMM-MensaMenu.js
@@ -1,10 +1,10 @@
-// MMM-MensaMenu.js
 Module.register("MMM-MensaMenu", {
     defaults: {
         url: "https://www.mensaplan.de/zweibruecken/mensa-zweibruecken/index.html"
     },
 
     start: function() {
+        this.menu = null;
         this.sendSocketNotification("GET_MENU", this.config.url);
     },
 
@@ -16,6 +16,31 @@ Module.register("MMM-MensaMenu", {
     },
 
     getDom: function() {
-        // Erstellen Sie hier Ihren DOM basierend auf `this.menu`
+        var wrapper = document.createElement("div");
+        if (this.menu) {
+            var table = document.createElement("table");
+            var header = document.createElement("header");
+            header.innerHTML = "Mensaspeiseplan";
+            wrapper.appendChild(header);
+
+            var today = new Date().getDay();
+            if (today === 6) { // Samstag
+                wrapper.innerHTML = "Die Mensa ist am Samstag geschlossen.";
+                return wrapper;
+            }
+
+            if (this.menu.closed) {
+                wrapper.innerHTML = "Die Mensa ist geschlossen.";
+                return wrapper;
+            }
+
+            // Hier die Tabelle mit dem Menü aufbauen
+            // ...
+
+            wrapper.appendChild(table);
+        } else {
+            wrapper.innerHTML = "Lade Menü...";
+        }
+        return wrapper;
     }
 });
--- node_helper.js
+++ node_helper.js
@@ -13,10 +13,19 @@ module.exports = NodeHelper.create({
         request(url, (error, response, body) => {
             if (!error && response.statusCode == 200) {
                 var $ = cheerio.load(body);
-                var menu = [];
-                // Füllen Sie das `menu` Array basierend auf der gescrapten HTML-Struktur
+                var menu = this.parseMenu($);
                 this.sendSocketNotification("MENU_RESULT", menu);
+            } else {
+                this.sendSocketNotification("MENU_RESULT", { error: true });
             }
         });
+    },
+
+    parseMenu: function($) {
+        var menu = { closed: false, items: [] };
+        // Hier die Logik zum Parsen des Menüs einfügen
+        // ...
+
+        return menu;
     }
 });
