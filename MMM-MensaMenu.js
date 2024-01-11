f675e323592aa95698c4914a6e98ad517bf29e73
kaza1001 <kaza1001@stud-hskl.de>
2024-01-11 15:57:33 +0100

    menu

--- MMM-MensaMenu.js
+++ MMM-MensaMenu.js
@@ -17,21 +17,33 @@ Module.register("MMM-MensaMenu", {
   },
 
   getDom: function () {
-    var wrapper = document.createElement("div");
-    if (!this.food.Monday) {
-      wrapper.innerHTML = "Loading menu...";
-      wrapper.className = "dimmed light small";
-      return wrapper;
-    }
+    var wrapper = document.createElement("table");
+    wrapper.className = "small";
 
-    var today = new Date().getDay() - 1; // 0 for Sunday, 1 for Monday, etc.
+    var headerRow = document.createElement("tr");
     var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-    var day = days[today];
-    if (day) {
-      wrapper.innerHTML = `<strong>${day}:</strong><br>${this.food[day]}`;
-    } else {
-      wrapper.innerHTML = 'Am Wochenende hat die Mensa leider geschlossen! ;)';
-    }
+    days.forEach(day => {
+      var headerCell = document.createElement("th");
+      headerCell.innerHTML = day;
+      headerRow.appendChild(headerCell);
+    });
+    wrapper.appendChild(headerRow);
+
+    var food1Row = document.createElement("tr");
+    days.forEach(day => {
+      var food1Cell = document.createElement("td");
+      food1Cell.innerHTML = this.food[day] ? this.food[day].split(" ")[0] : "";
+      food1Row.appendChild(food1Cell);
+    });
+    wrapper.appendChild(food1Row);
+
+    var food2Row = document.createElement("tr");
+    days.forEach(day => {
+      var food2Cell = document.createElement("td");
+      food2Cell.innerHTML = this.food[day] ? this.food[day].split(" ")[1] : "";
+      food2Row.appendChild(food2Cell);
+    });
+    wrapper.appendChild(food2Row);
 
     return wrapper;
   },
