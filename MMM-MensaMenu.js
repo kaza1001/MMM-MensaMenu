d557be2ae4e75241bdb36951af8eb4d38e2621d6
kaza1001 <151996682+kaza1001@users.noreply.github.com>
2024-01-17 15:01:44 +0100

    Update MMM-MensaMenu.js

--- MMM-MensaMenu.js
+++ MMM-MensaMenu.js
@@ -17,39 +17,43 @@ Module.register("MMM-MensaMenu", {
   },
 
   getDom: function () {
-    var wrapper = document.createElement("div");
-    wrapper.className = "menu-wrapper";
+  var wrapper = document.createElement("div");
+  wrapper.className = "menu-wrapper";
 
-    var title = document.createElement("div");
-    title.innerHTML = "HSKL Zweibrücken Mensa-Speiseplan";
-    title.className = "menu-title";
-    wrapper.appendChild(title);
+  var title = document.createElement("div");
+  title.innerHTML = "HSKL Zweibrücken Mensa-Speiseplan";
+  title.className = "menu-title";
+  wrapper.appendChild(title);
 
-    var table = document.createElement("table");
-    table.className = "menu-table";
-    if (!this.food.Monday) {
-      table.innerHTML = "Loading menu...";
-      table.className = "dimmed light small";
-      wrapper.appendChild(table);
-      return wrapper;
-    }
-
-    var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-    days.forEach(day => {
-      var dayRow = table.insertRow();
-      var cell = dayRow.insertCell();
-      cell.innerHTML = day;
-
-      this.food[day].forEach(meal => {
-        var mealRow = table.insertRow();
-        cell = mealRow.insertCell();
-        cell.innerHTML = meal;
-      });
-    });
-
+  var table = document.createElement("table");
+  table.className = "menu-table";
+  if (!this.food.Monday) {
+    table.innerHTML = "Loading menu...";
+    table.className = "dimmed light small";
     wrapper.appendChild(table);
     return wrapper;
-  },
+  }
+
+  var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+
+  days.forEach(day => {
+    var daysRow = table.insertRow();
+    var dayCell = daysRow.insertCell();
+    dayCell.innerHTML = day;
+
+    var firstMealRow = table.insertRow();
+    var firstMealCell = firstMealRow.insertCell();
+    firstMealCell.innerHTML = this.food[day][0]; // Erstes Essen
+
+    var secondMealRow = table.insertRow();
+    var secondMealCell = secondMealRow.insertCell();
+    secondMealCell.innerHTML = this.food[day][1]; // Zweites Essen
+  });
+
+  wrapper.appendChild(table);
+  return wrapper;
+},
+
 
   getMenu: function () {
     this.sendSocketNotification("GET_MENU");
